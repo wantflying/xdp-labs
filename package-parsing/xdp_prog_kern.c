e02cb1f2546a8ef433d327fcb3e150b249662460
wantflying <99820811+wantflying@users.noreply.github.com>
2024-09-11 18:55:04 +0800

    添加ipv4
    丢弃所有icmp报文

--- package-parsing/xdp_prog_kern.c
+++ package-parsing/xdp_prog_kern.c
@@ -5,6 +5,7 @@
 #include <linux/if_ether.h>
 #include <linux/if_packet.h>
 #include <linux/ipv6.h>
+#include <linux/ip.h>
 #include <linux/icmpv6.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
@@ -16,7 +17,11 @@
 struct hdr_cursor {
 	void *pos;
 };
-
+	//vlan头
+struct vlan_hdr {
+	__be16	h_vlan_TCI;
+	__be16	h_vlan_encapsulated_proto;
+};
 /* Packet parsing helpers.
  *
  * Each helper parses a packet header, including doing bounds checking, and
@@ -26,6 +31,12 @@ struct hdr_cursor {
  * (h_proto for Ethernet, nexthdr for IPv6), for ICMP it is the ICMP type field.
  * All return values are in host byte order.
  */
+static __always_inline int proto_is_vlan(__u16 h_proto)
+{
+        return !!(h_proto == bpf_htons(ETH_P_8021Q) ||
+                  h_proto == bpf_htons(ETH_P_8021AD));
+}
+
 static __always_inline int parse_ethhdr(struct hdr_cursor *nh,
 					void *data_end,
 					struct ethhdr **ethhdr)
@@ -47,8 +58,28 @@ static __always_inline int parse_ethhdr(struct hdr_cursor *nh,
     // bpf_trace_printk("execve called with parse ethhdr: %u\n", ethhdrsize);
 	nh->pos += ethhdrsize;
 	// *ethhdr = eth;
+	__u16 h_proto = (*ethhdr)->h_proto;
 
-	return (*ethhdr)->h_proto; /* network-byte-order */
+
+	struct vlan_hdr *vlh;
+	vlh = nh->pos;
+	int i;
+	//处理vlan标记
+	#pragma unroll
+	for (i = 0; i < 2; i++) {
+		if (!proto_is_vlan(h_proto))
+			break;
+		//判断是否越界
+		if (vlh + 1 > data_end)
+			break;
+		h_proto = vlh->h_vlan_encapsulated_proto;
+		//指针移动动下一个头起点
+		vlh++;
+	}
+
+	nh->pos = vlh;
+
+	return h_proto; /* network-byte-order */
 }
 
 /* Assignment 2: Implement and use this */
@@ -65,6 +96,26 @@ static __always_inline int parse_ip6hdr(struct hdr_cursor *nh,
 	bpf_printk("Received packet with icmp protocol: %u,ipv6size:%u %u\n", ip6->nexthdr,ipv6hdrsize,bpf_htons(IPPROTO_ICMPV6));
 	return ip6->nexthdr;
 
+}
+
+/* 处理ipv4报文 */
+static __always_inline int parse_iphdr(struct hdr_cursor *nh,
+					void *data_end,
+					struct iphdr **iphdr)
+{
+	*iphdr = nh->pos;
+	if((*iphdr)+1 > data_end){
+		return -1;
+	}
+	int iphdrsize = (*iphdr)->ihl * 4;
+	//ipv4 header ihl*4 表明了header的大小，这个大小要不能大于iphdrsize
+	if(iphdrsize > sizeof(**iphdr)){
+		return -1;
+	}
+
+	nh->pos += iphdrsize;
+
+	return (*iphdr)->protocol;
 }
 
 /* Assignment 3: Implement and use this */
@@ -107,6 +158,16 @@ int  xdp_parser_func(struct xdp_md *ctx)
 	 * header type in the packet correct?), and bounds checking.
 	 */
 	nh_type = parse_ethhdr(&nh, data_end, &eth);
+	//检查是否是ip报文
+	if(nh_type == bpf_htons(ETH_P_IP)){
+		struct  iphdr *ip;
+		//检查是否是icmp报文,如果是则丢弃
+		if(parse_iphdr(&nh, data_end, &ip) == IPPROTO_ICMP){
+					action = XDP_DROP;
+		}
+		goto out;
+	}
+	
 	if (nh_type != bpf_htons(ETH_P_IPV6))
 		goto out;
 	/* Assignment additions go below here  IPPROTO_ICMPV6*/ 
